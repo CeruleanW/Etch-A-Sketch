8dba904964b49e6b3ca64efb437ba5184935d1ba
CeruleanW <yangyi6463@gmail.com>
2020-06-21 16:47:31 -0400

    Updates

---
+++ precache-manifest.1ad14d02b9d240291e6b52898e30cd08.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9659a08e0dba220fca1fbf53ca71de56",
+    "url": "/etch-a-sketch/index.html"
+  },
+  {
+    "revision": "6cb2558e05659fc1c4f6",
+    "url": "/etch-a-sketch/static/css/main.1676c273.chunk.css"
+  },
+  {
+    "revision": "7bb3ae4afa42f333aaa6",
+    "url": "/etch-a-sketch/static/js/2.cac89be4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/etch-a-sketch/static/js/2.cac89be4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cb2558e05659fc1c4f6",
+    "url": "/etch-a-sketch/static/js/main.04db05f9.chunk.js"
+  },
+  {
+    "revision": "a321b3642bc96b1c08c8",
+    "url": "/etch-a-sketch/static/js/runtime-main.a0b1b257.js"
+  },
+  {
+    "revision": "a996f3b81322eedad91c571f03ce6b64",
+    "url": "/etch-a-sketch/static/media/alexbrush-regular-webfont.a996f3b8.woff2"
+  },
+  {
+    "revision": "ded9337036d696770b5a7032fa1d58ab",
+    "url": "/etch-a-sketch/static/media/alexbrush-regular-webfont.ded93370.woff"
+  }
+]);
